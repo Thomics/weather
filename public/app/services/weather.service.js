1a684daa8b3061811c473878708297fff54cabe2
Thomics <contot@gmail.com>
2016-09-25 21:33:31 -0700

    adding geolocation

--- public/app/services/weather.service.js
+++ public/app/services/weather.service.js
@@ -12,6 +12,7 @@
 
     var vm = this;
 
+    vm.getForecast = getForecast;
     vm.getWeather = getWeather;
 
 
@@ -19,6 +20,9 @@
       return $http.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + ",us&appid=b1b15e88fa797225412429c1c50c122a1&apikey=37aded37883e894c15de78755ecb3b6d&units=imperial");
     }
 
+    function getForecast(city) {
+      return $http.get('http://api.openweathermap.org/data/2.5/forecast?q=' + city + ",us&appid=b1b15e88fa797225412429c1c50c122a1&apikey=37aded37883e894c15de78755ecb3b6d&units=imperial");
+    }
 
 
   }
--- public/app/weather.controller.js
+++ public/app/weather.controller.js
@@ -11,12 +11,51 @@
 
     var vm = this;
 
+    vm.getCoordinates = getCoordinates;
+    vm.getForecast = getForecast;
     vm.getWeather = getWeather;
 
+    vm.forecast = [];
+    vm.coords = {};
     vm.temp;
     vm.wind;
 
-    vm.getWeather('seattle');
+    activate();
+
+    function activate() {
+
+      vm.getWeather('Seattle');
+      vm.getForecast('Seattle');
+      console.log(vm.forecast);
+
+      vm.getCoordinates();
+
+    }
+
+    function getCoordinates() {
+      if ( navigator.geolocation ) {
+        navigator.geolocation.getCurrentPosition(function(pos){
+          console.log(pos);
+
+          vm.coords.lat = pos.latitude;
+          vm.coords.long = pos.longitude;
+
+        });
+      } else {
+        alert('you blew it');
+      }
+    }
+
+    function getForecast(city) {
+      WeatherService.getForecast(city)
+        .success(function (data) {
+          console.log(data);
+          vm.forecast = generateForecast(data.list);
+        }).error(function (err) {
+          console.log(err);
+        });
+    }
+
 
     function getWeather(city) {
       WeatherService.getWeather(city)
@@ -27,6 +66,20 @@
         }).error(function (err) {
           console.log(err);
         });
+    }
+
+
+    function generateForecast(arr) {
+      var forecast = [];
+      for ( var i = 0; i < arr.length; i++ ) {
+        forecast.push(
+          {
+            time: arr[i].dt_txt,
+            temp: arr[i].main.temp
+          });
+      }
+      console.log(forecast);
+      return forecast;
     }
 
   }
