d898df7076daa8fee7f1dfcc303185eb432d8f66
Thomics <contot@gmail.com>
2017-09-11 22:32:56 -0700

    Created function for refreshing locations

--- public/app/controllers/weather.controller.js
+++ public/app/controllers/weather.controller.js
@@ -12,9 +12,12 @@
     var vm = this;
 
     vm.getWeather = getWeather;
+    vm.updateWeather = updateWeather;
     vm.icon;
+    vm.id;
     vm.weatherType;
     vm.location = "Seattle";
+    vm.locationArr = [];
     vm.weatherArr = [];
 
   //////////////////////////////////////////////////////////////////////////////////////////
@@ -29,19 +32,59 @@
           vm.time = new Date().getHours();
           vm.icon = IconService.getIcon(data.weather[0].id, vm.time);
           vm.weatherType = IconService.getWeatherType(data.weather[0].id);
+          vm.id = data.id;
 
 
           vm.weatherArr.push({
             location: vm.location,
+            id: vm.id,
             temp: vm.temp,
             icon: vm.icon,
             weatherType: vm.weatherType
           });
 
+          vm.locationArr.push(vm.id);
+
 
         }).error(function (err) {
           console.log(err);
         });
+    }
+
+
+    function updateWeather() {
+
+      var locationIDs = vm.locationArr.join(',');
+
+      WeatherService.getWeatherByID(locationIDs)
+        .success(function (data) {
+
+          console.log(data);
+          var tempWeatherArr = [];
+
+          for ( var i = 0; i < data.list.length; i++ ) {
+
+
+            tempWeatherArr.push({
+              location: data.list[i].name,
+              id: data.list[i].id,
+              temp: data.list[i].main.temp,
+              icon: IconService.getIcon(data.list[i].weather[0].id, vm.time), //Update time to be local time
+              weatherType: IconService.getWeatherType(data.list[i].weather[0].id)
+            });
+
+          }
+
+          console.log(tempWeatherArr);
+
+          vm.weatherArr = tempWeatherArr;
+
+
+        }).error(function (err) {
+          console.log(err);
+        });
+
+
     }
 
   }
