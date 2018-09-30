2ce36b3719e2ce08117bfdc7e66825e3bcf3ba36
Thomas Conto <contot@gmail.com>
2018-09-30 01:09:49 -0700

    Update http to https for api request

--- public/app/services/weather.service.js
+++ public/app/services/weather.service.js
@@ -17,11 +17,11 @@
 
 
     function getWeather(city) {
-      return $http.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + ",us&appid=b1b15e88fa797225412429c1c50c122a1&apikey=37aded37883e894c15de78755ecb3b6d&units=imperial");
+      return $http.get('https://api.openweathermap.org/data/2.5/weather?q=' + city + ",us&appid=b1b15e88fa797225412429c1c50c122a1&apikey=37aded37883e894c15de78755ecb3b6d&units=imperial");
     }
 
     function getWeatherByID(cities) {
-      return $http.get('http://api.openweathermap.org/data/2.5/group?id=' + cities + "&appid=b1b15e88fa797225412429c1c50c122a1&apikey=37aded37883e894c15de78755ecb3b6d&units=imperial");
+      return $http.get('https://api.openweathermap.org/data/2.5/group?id=' + cities + "&appid=b1b15e88fa797225412429c1c50c122a1&apikey=37aded37883e894c15de78755ecb3b6d&units=imperial");
     }
 
   }
